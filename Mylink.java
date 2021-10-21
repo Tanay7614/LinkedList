8f26ce7c9822eeb855f7102905905dd8cdf4909e
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-21 21:10:18 +0530

    create a linked list

---
+++ Mylink.java
@@ -0,0 +1,16 @@
+package sample2;
+
+
+
+public class Mylink {
+	
+	 public void insert() {
+		   Node first=new Node(30);
+		   Node second=new Node(56);
+		   Node third=new Node(70);
+		   first.next=second;
+		   second.next=third;
+		  
+	   }
+
+}
