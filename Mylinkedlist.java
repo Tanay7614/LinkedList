61759618f24ac6c1eeb53516847f45aa887caf0a
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 15:35:17 +0530

    It define all methods

--- Mylinkedlist.java
+++ Mylinkedlist.java
@@ -4,55 +4,59 @@ public class Mylinkedlist {
      Node head;
      Node tail;
 	
-     public Mylinkedlist() {
-		
-		this.head = null;
-		this.tail=null;
-	}
+     public Mylinkedlist()
+     {
+	 this.head = null;
+         this.tail=null;
+    }
 	
-     public void add(Node newNode) {
-		 if(head==null) {
-			   head=newNode;
-			  
-		 }
-	     if(tail==null) {
-			 tail=newNode;
-	     }
-	     else {
+     public void add(Node newNode)
+      {
+	   if(head==null)
+	   {
+		 head=newNode;
+	   }
+	   if(tail==null)
+	   {
+	        tail=newNode;
+	   }
+	   else
+	   {
 	    	Node temp = head;
 	    	head=newNode;
 	    	head.next=temp;
 	    	System.out.println(temp.data);
 	    	
-	     } 
-  }
+	   } 
+       }
      
-     public void append(Node newNode) {
-    	 if(head==null && tail==null) {
-			   head=newNode;
-			   tail=newNode;
+     public void append(Node newNode) 
+      {
+    	 if(head==null && tail==null)
+	 {
+	     head=newNode;
+             tail=newNode;
 			   
-		 }
-	    
-	     else {
-	    	// Node tp=head;
-	    	 Node temp=newNode;
+	 }
+	 else {
+	         Node temp=newNode;
 	    	 tail.next=newNode;
 	    	 tail=temp;
-	    	// System.out.println(.data);
-	     }
+	      }
  		
- 	}
+       }
 	
-     public void print() {
-		Node temp=head;
-		while(temp.next!=null) {
-			System.out.println(temp.data);
-			temp=temp.next;
-		}
-		System.out.println(temp.data);
+     public void print()
+     {  
+	    Node temp=head;
+	    while(temp.next!=null)
+	    {
+                 System.out.println(temp.data);
+		 temp=temp.next;
+	    }
+	    System.out.println(temp.data);
 			
-	}
+     }
 
 }
      
