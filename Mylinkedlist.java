a374973475feca3c9b61156b91192d7a99756f2b
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 15:29:59 +0530

    Define all methods

---
+++ Mylinkedlist.java
@@ -0,0 +1,97 @@
+package sample2;
+
+     public class Mylinkedlist {
+     Node head;
+     Node tail;
+	
+    public Mylinkedlist()
+    {
+	    this.head = null;
+		this.tail=null;
+	}
+	
+    public void add(Node newNode)
+     {
+		 if(head==null) {
+			head=newNode;
+		 }
+	    
+		 if(tail==null) {
+			 tail=newNode;
+	     }
+	    
+		 else {
+	    	Node temp = head;
+	    	head=newNode;
+	    	head.next=temp;
+	    	
+	     } 
+     }
+     
+     public void append(Node newNode) 
+     {
+    	 if(head==null && tail==null) 
+    	 {
+			   head=newNode;
+			   tail=newNode;
+			   
+		 }
+	    
+	     else {
+	    	
+	    	 Node temp=newNode;
+	    	 tail.next=newNode;
+	    	 tail=temp;
+	    }
+    }
+    
+     public void insertBetween(Node newNode)
+     {
+ 		Node temp=head;
+ 		temp.next=newNode;
+ 		newNode.next=tail;
+   	 }
+     
+     public void delete(Node first) 
+     {
+ 		Node temp=head;
+ 		head=temp.next;
+ 		
+   	 }
+     
+     public void deleteLast() {
+ 		Node temp=head;
+ 		tail=temp.next;
+ 		
+ 		
+ 	}
+     
+     public void search( int key) {
+    	 Node temp=head;
+    	 while(temp.next!=null) {
+    		 if(temp.data==key) {
+    			 System.out.println("value "+key+" is present");
+    		 }
+    	    temp=temp.next;
+         }
+    	 if(temp.data==key) {
+    		 System.out.println("value "+key+" is present"); 
+    	 }
+    	
+     }
+	
+     public void print()
+     {
+		Node temp=head;
+		while(temp.next!=null)
+		{
+			System.out.println(temp.data);
+			temp=temp.next;
+		}
+		System.out.println(temp.data);
+			
+	 }
+
+  }
+     
+
