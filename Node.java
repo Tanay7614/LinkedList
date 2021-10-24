f02e9a071857003b7621036c6390444fdaf44edd
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 15:25:34 +0530

    Node create

--- Node.java
+++ Node.java
@@ -3,18 +3,13 @@ package sample2;
 public class Node {
 	
 	public  int data;
-    public   Node next;
+        public   Node next;
 	public Node(int data) {
 		
 		this.data = data;
 		this.next = null;
 	}
-	public int getData() {
-		return data;
-	}
-	public void setData(int data) {
-		this.data = data;
-	}
+	
 	public Node getNext() {
 		return next;
 	}
