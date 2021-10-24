e510c551c86099e8a9d0d9e5c2c13f7eafa8c3be
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 15:24:05 +0530

    Main method

--- Main.java
+++ Main.java
@@ -1,11 +1,7 @@
 package sample2;
-
-
-
 public class Main {
 	 public static void main(String[] args) {
   	 Mylink list=new  Mylink();
-		   
-		    list.insert();
-		 }
+         list.insert();
+     }
 }
