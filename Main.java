1cd6afcd0ad5a365ee14bded95dd15cca8174805
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-24 18:05:24 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 package sample2;
-
-
-
 public class Main {
-	 public static void main(String[] args) {
+	 public static void main(String[] args)
+	 {
   	 Mylink list=new  Mylink();
-		   
-		    list.insert();
-		 }
+	 list.insert();
+	 }
 }
