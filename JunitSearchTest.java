7f7d2f849ffe402dac93289fd1fcfc7631ba4657
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-29 20:51:26 +0530

    Method declaration

--- JunitSearchTest.java
+++ JunitSearchTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 class JunitSearchTest {
 	public final Mylinkedlist list1 = new Mylinkedlist();
 	@Test
-	void test() {
+	void test()
+	{
 		
 		   Node first=new Node(30);
 		   Node second=new Node(56);
@@ -16,11 +17,7 @@ class JunitSearchTest {
 		   list1.append(third);
 		 
 		   list1.search(30);
-		 //  list1.insertBetween();
+		   list1.insertBetween();
+		   list1.sort();
 		   list1.delete();
-		   list1.print();
-		  
-		   
 	}
-
-}
