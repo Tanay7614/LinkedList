17663626d44a319713d183f28bac2cdf2735cf47
Tanay7614 <86945887+Tanay7614@users.noreply.github.com>
2021-10-29 17:43:24 +0530

    Node class

--- Node.java
+++ Node.java
@@ -3,23 +3,20 @@ package sample2;
 public class Node {
 	
 	public  int data;
-    public   Node next;
-	public Node(int data) {
+        public   Node next;
+	public Node(int data) 
+	{
 		
 		this.data = data;
 		this.next = null;
 	}
-	public int getData() {
+	public int getData()
+	{
 		return data;
 	}
-	public void setData(int data) {
+	public void setData(int data)
+	{
 		this.data = data;
-	}
-	public Node getNext() {
-		return next;
-	}
-	public void setNext(Node next) {
-		this.next = next;
 	}
 	
 }
